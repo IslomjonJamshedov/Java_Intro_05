6acaef0da50f1957d8b210f1e0231e21bee87419
Islomjon Jamshedov <jonjamshedov@gmail.com>
2022-11-08 19:24:53 -0600

    added new codes

--- src/regex/MatcherClass.java
+++ src/regex/MatcherClass.java
@@ -1,7 +1,13 @@
 package regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MatcherClass {
     public static void main(String[] args) {
+    Pattern patter = Pattern.compile("a-zA-Z0-9{5,10}");
+    Matcher matcher = patter.matcher("hello");
 
+        System.out.println(matcher.matches());
     }
 }
